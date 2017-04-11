fe7942e199a1a751b47a7068e844374aa8894255
lucasmidlhey <lucasmidlhey@hotmail.com>
2017-04-11 18:25:42 +0000

    create task models
    Signed-off-by: lucasmidlhey <lucasmidlhey@hotmail.com>

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -23,13 +23,23 @@ RSpec.describe TasksController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Task. As you add validations to Task, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  before(:all) do
+      @user = FactoryGirl.create :user
+      @user.save
+      @category = FactoryGirl.create :category
+      @category.save
+      @book = FactoryGirl.create :category
+      @book.save
+  end
+    let(:valid_attributes) {
+    {title: 'testTask', user_id: @user.id, category_id: @category.id, book_id: @book.id}
+    }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+    let(:invalid_attributes) {
+    {title: nil, user_id: nil, category_id: nil,    book_id: nil}
+    }
+
+
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,4 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  before(:each) do
+    @task = FactoryGirl.create :task
+  end
+  it "Saves a task with valid attributes" do
+    expect(@task.save).to be (true)
+  end
+  describe "Title validations" do
+
+    it "Won't save a task without a Title" do
+      @task.title = ""
+      expect(@task.save).not_to be (true)
+      expect(@task.errors[:title]).to include("can't be blank")
+      @task.title = "Valid Title"
+      expect(@task.save).to be(true)
+    end
+
+    it "Won't save a user with invalid name length" do
+      @task.title = "Test"
+      expect(task.save).not_to be(true)
+      expect(@task.errors[:title]).to include("is too short (minimum is 5 characters)")
+
+      @task.title = "a"*200
+      expect(@task.save).not_to be(true)
+      expect(@task.errors[:name]).to include("is too long (maximum is 200 characters)")
+
+      @task.title = "Valid Name"
+      expect(@user.save).to be(true)
+    end
+
+    end
+
 end
