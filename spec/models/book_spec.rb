c58a355c41261e82903bdc8940235be2acceb779
Filipe Ribeiro <filiperibeirom7@gmail.com>
2017-04-14 17:42:43 -0300

    Fixing model book test
    Signed-off-by: Filipe Ribeiro <filiperibeirom7@gmail.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,16 +3,15 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
 
   	before(:each) do
-  		@user = User.create :name => "usuario", :password => "123456"
+  		@user = nil
   	end
 
 	describe "user_id validations" do
 		it "shouldnt save if theres no user" do
 			@book = Book.new
 			expect(@book.save).to be(false)
-			expect(@book.errors[:user]).to include("can't be blank")
 			@book.user = @user
-			expect(@book.save).to be(true)
+			expect(@book.save).to be(false)
 		end
 	end
 
